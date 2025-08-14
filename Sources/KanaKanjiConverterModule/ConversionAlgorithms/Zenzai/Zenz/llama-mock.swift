d2a35be16841f4a5511e6cb577d01019a1554dcc
Nanaka Hiira <hiira@hiira.dev>
2025-08-15 08:31:51 +0900

    llama-mock: add n_gpu_layers

--- Sources/KanaKanjiConverterModule/ConversionAlgorithms/Zenzai/Zenz/llama-mock.swift
+++ Sources/KanaKanjiConverterModule/ConversionAlgorithms/Zenzai/Zenz/llama-mock.swift
@@ -31,6 +31,7 @@ package func llama_backend_free() {}
 
 package struct llama_model_params {
     package var use_mmap: Bool
+    package var n_gpu_layers: Int
 }
 package func llama_model_default_params() -> llama_model_params { unimplemented() }
 
